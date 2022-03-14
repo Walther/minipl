2e1c061781a247a7df46178c83c262603c081d24
Walther <veeti.haapsamo@gmail.com>
2022-03-14 02:43:42 +0200

    Testing: more integration testing

--- tests/integration.rs
+++ tests/integration.rs
@@ -61,5 +61,47 @@ mod valid {
 
 #[cfg(test)]
 mod invalid {
-    // TODO: integration tests for invalid code
+    use miette::{GraphicalReportHandler, GraphicalTheme, Report};
+    use minipl::lexing::scan;
+    use minipl::parsing::{parse, ParseError};
+    use minipl::visitors::Interpreter;
+
+    // TODO: even better UI testing
+    fn fmt_report(diag: Report) -> String {
+        let mut out = String::new();
+
+        GraphicalReportHandler::new_themed(GraphicalTheme::unicode_nocolor())
+            .with_width(512)
+            .render_report(&mut out, diag.as_ref())
+            .unwrap();
+
+        out
+    }
+
+    #[test]
+    fn re_declaration() {
+        let source = include_str!("sources/invalid/re_declaration.minipl");
+        let tokens = scan(source).unwrap();
+        let parsed = parse(tokens).unwrap();
+        let mut interpreter = Interpreter::new();
+
+        if let Err(report) = interpreter.eval(&parsed) {
+            let report = fmt_report(report);
+            dbg!(&report);
+            assert!(report.contains("Variable error"));
+            assert!(report.contains(
+                "Try removing the latter `var` to reassign, or use a different identifier"
+            ));
+        } else {
+            panic!()
+        };
+    }
+
+    #[test]
+    fn var_equal_not_walrus() {
+        let source = include_str!("sources/invalid/var_equal_not_walrus.minipl");
+        let tokens = scan(source).unwrap();
+        let result = parse(tokens);
+        assert!(matches!(result, Err(ParseError::ExpectedWalrus(_))));
+    }
 }
